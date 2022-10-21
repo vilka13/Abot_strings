be186a9ecfe209a4970beb6533879712ac1305f7
Stanisław <112472746+vilka13@users.noreply.github.com>
2022-10-21 11:25:54 +0200

    Create main.py

---
+++ string/main.py
@@ -0,0 +1,35 @@
+greeting = "hello"
+first_name = "My"
+last_name = "Friend"
+exclamation_sign = "!"
+white_space = " "
+whole_write = greeting+white_space+first_name+white_space+last_name+white_space+exclamation_sign
+print(whole_write)
+
+#Escaping
+string_with_new_line = "\n\thello \n\tGITHUB!"
+print(string_with_new_line)
+
+some_string = "I'm programmer"
+print(some_string)
+
+another_string = 'I want to learn "Python"'
+print(another_string)
+
+the_third_line = 'I\'m a novice python user,want to become a professional programmer!'
+print(the_third_line)
+
+ps= "In these lines, I used everything I know about strings."
+print(ps)
+
+string_with_r = "My name is Stanislaw\n \rI'm from with Poland!"
+print(string_with_r)
+
+#triple quotes
+
+string_with_triple = """This test strings with "triple quotes" """
+print(string_with_triple)
+
+day = "Have i nice day!\t;)"
+print(day)
+
